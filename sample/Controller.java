6cac6018d95e58122596f7057847cf51087966d8
Nutsmb <airat.kharrasov@gmail.com>
2021-07-05 16:27:41 +0300

    Add form for discharge parameters

---
+++ sample/Controller.java
@@ -0,0 +1,37 @@
+package sample;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
+
+public class Controller {
+
+    @FXML
+    TextField time;
+
+    @FXML
+    TextField dischargeN;
+
+    @FXML
+    TextField gas;
+
+    @FXML
+    TextField pressure;
+    @FXML
+    TextField dischargeU;
+
+    @FXML
+    TextField dipAmpl;
+
+    @FXML
+    TextField dipTime;
+
+    @FXML
+    Button addToDB;
+
+    public void addDischargeToDB(ActionEvent actionEvent) {
+
+
+    }
+}
